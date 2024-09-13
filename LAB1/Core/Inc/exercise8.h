5e3aa97abf751b9257a2a15f35b073e98b9ffe21
ThinhLe\Admin <thinhphuclenguyen2502@gmail.com>
2024-09-13 17:19:47 +0700

    Finalize Ex8

---
+++ LAB1/Core/Inc/exercise8.h
@@ -0,0 +1,14 @@
+/*
+ * exercise8.h
+ *
+ *  Created on: Sep 13, 2024
+ *      Author: Admin
+ */
+
+#ifndef INC_EXERCISE8_H_
+#define INC_EXERCISE8_H_
+#include "main.h"
+
+void clearAllClock();
+void setNumberOnClock(int num);
+#endif /* INC_EXERCISE8_H_ */
--- LAB1/Core/Src/exercise8.c
+++ LAB1/Core/Src/exercise8.c
@@ -1,10 +1,10 @@
 /*
- * exercise6.c
+ * exercise8.c
  *
  *  Created on: Sep 13, 2024
  *      Author: Admin
  */
-#include "exercise6.h"
+#include "exercise8.h"
 
 void clearAllClock()
 {
@@ -21,9 +21,9 @@ void clearAllClock()
 	HAL_GPIO_WritePin(LED10_GPIO_Port, LED10_Pin, RESET);
 	HAL_GPIO_WritePin(LED11_GPIO_Port, LED11_Pin, RESET);
 }
-void turnOn(int pinLED)
+void setNumberOnClock(int num)
 {
-	switch(pinLED)
+	switch(num)
 	{
 	case 0:
 		HAL_GPIO_WritePin(LED0_GPIO_Port, LED0_Pin, SET);
@@ -60,50 +60,6 @@ void turnOn(int pinLED)
 		break;
 	case 11:
 		HAL_GPIO_WritePin(LED11_GPIO_Port, LED11_Pin, SET);
-		break;
-	default:
-		break;
-	}
-}
-void turnOff(int pinLED)
-{
-	switch(pinLED)
-	{
-	case 0:
-		HAL_GPIO_WritePin(LED0_GPIO_Port, LED0_Pin, RESET);
-		break;
-	case 1:
-		HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, RESET);
-		break;
-	case 2:
-		HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, RESET);
-		break;
-	case 3:
-		HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, RESET);
-		break;
-	case 4:
-		HAL_GPIO_WritePin(LED4_GPIO_Port, LED4_Pin, RESET);
-		break;
-	case 5:
-		HAL_GPIO_WritePin(LED5_GPIO_Port, LED5_Pin, RESET);
-		break;
-	case 6:
-		HAL_GPIO_WritePin(LED6_GPIO_Port, LED6_Pin, RESET);
-		break;
-	case 7:
-		HAL_GPIO_WritePin(LED7_GPIO_Port, LED7_Pin, RESET);
-		break;
-	case 8:
-		HAL_GPIO_WritePin(LED8_GPIO_Port, LED8_Pin, RESET);
-		break;
-	case 9:
-		HAL_GPIO_WritePin(LED9_GPIO_Port, LED9_Pin, RESET);
-		break;
-	case 10:
-		HAL_GPIO_WritePin(LED10_GPIO_Port, LED10_Pin, RESET);
-		break;
-	case 11:
-		HAL_GPIO_WritePin(LED11_GPIO_Port, LED11_Pin, RESET);
 		break;
 	default:
 		break;
